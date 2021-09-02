c27a420cfb237cd1c18bf24785614d32a84e865b
chemacenturion <centurion.philadelphia@gmail.com>
2021-09-02 10:37:09 -0400

    tutor assisted troubleshoot error corrected

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,11 +42,13 @@ router.post('/', async (req, res) => {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
+      // ***** needs category_id:!!!!!
       tagIds: [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
     .then((product) => {
+      // console.log(product); troubleshooting why product route was returning 400 err
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
